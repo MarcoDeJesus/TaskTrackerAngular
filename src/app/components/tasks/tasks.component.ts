5e05f568dda1edbda411e0a29a2458885cb11949
Marco De Jesus <marcodejesus2010@gmail.com>
2022-08-09 21:37:55 -0500

    Adding a task functionality

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -29,4 +29,8 @@ export class TasksComponent implements OnInit {
     //console.log(task.reminder);
     this.taskService.updateTaskReminder(task).subscribe();
   }
+
+  addTask(task: Task){
+    console.log(task);
+  }
 }
